21f25b9a1c63d40423e8503cd51e1d1d3ad7dea3
Jim Paraschakis <140411754+JimP-lab@users.noreply.github.com>
2024-08-20 12:42:14 +0300

    Update verification-model.php

--- verification-model.php
+++ verification-model.php
@@ -1,32 +1,24 @@
 <?php
 class MCMVerification_model extends CI_Model {
-    public function get_verifications() {
-        $query = $this->db->get('tblads_MCMVerification');
-        return $query->result_array();
-    }
-
-    public function check_unique($mcmVendorID, $demandPartnerID) {
-        $this->db->where('MCMVendorID', $mcmVendorID);
-        $this->db->where('DemandPartnerID', $demandPartnerID);
-        $query = $this->db->get('tblads_MCMVerification');
-
-        return $query->num_rows() == 0;
-    }
-
-    public function insert_verification($data) {
-        $this->db->from('tblads_MCMVendor', $data);
-        return $this->db->insert('tblads_MCMVerification', $data);
-    }
-
+    
     public function get_verification($id) {
         $this->db->where('ID', $id);
         $query = $this->db->get('tblads_MCMVerification');
         return $query->row_array();
     }
-
-    public function delete_verification($id) {
-        $this->db->where('ID', $id);
-        $this->db->delete('tblads_MCMVerification');
+    
+    public function insert_verification($data) {
+        $this->db->from('tblads_MCMVendor', $data);
+        return $this->db->insert('tblads_MCMVerification', $data);
     }
-}
+    
+    // public function check_unique($mcmVendorID, $demandPartnerID) {
+    //     $this->db->where('MCMVendorID', $mcmVendorID);
+    //     $this->db->where('DemandPartnerID', $demandPartnerID);
+    //     $query = $this->db->get('tblads_MCMVerification');
+
+    //     return $query->num_rows() == 0;
+    // }
+    
+   }
 ?>
